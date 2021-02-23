58a2626360bc88f8c37ffd927cf78a1db126c493
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-23 02:23:51 -0500

    Update Profile.java

--- src/Profile.java
+++ src/Profile.java
@@ -1,9 +1,33 @@
 package src;
 
 public class Profile {
-	private String name; //employees name in the form "lastname, firstname"
+	private String name; //employees name in the form "last name, first name"
 	private String department; // department code: CS, ECE, IT
 	private Date dateHired;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDept() {
+		return department;
+	}
+	
+	public void setDept(String dept) {
+		department = dept;
+	}
+	
+	public Date getDate( ) {
+		return dateHired;
+	}
+	
+	public void setDate(Date date) {
+		dateHired = date;
+	}
 	
 	@Override
 	public String toString() {
@@ -12,7 +36,17 @@ public class Profile {
 	
 	@Override
 	public boolean equals(Object obj) { //compare name, department, and date hired
-		return false;
+		if(obj == null)
+			return false;
+		
+		else if(obj instanceof Profile) {
+			Profile aProfile = (Profile)obj;
+			return name != aProfile.getName() ? false : (department != aProfile.getDept() ? false : (dateHired != aProfile.getDate() ? false : true));
+		}
+		
+		else 
+			return false;
+		
 	}
 
 }
