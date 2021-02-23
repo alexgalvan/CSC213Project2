098a2900c4de976af691f0a5cf6a15f7681b28ca
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-23 02:21:43 -0500

    Update Fulltime.java

--- src/Fulltime.java
+++ src/Fulltime.java
@@ -1,9 +1,37 @@
 package src;
 
 public class Fulltime extends Employee {
-
+	private int salary;
+	
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
 	@Override
-	public void CalculatePayment() {
+	public void calculatePayment() {
+		super.setPayment(getSalary()/26);
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	@Override
+	public boolean equals(Object obj) { //compare name, department, and date hired
+		if(obj == null)
+			return false;
+		
+		else if(obj instanceof Fulltime) {
+			Fulltime aFulltime = (Fulltime)obj;
+			return salary != aFulltime.getSalary() ? false : true;
+		}
+		
+		else 
+			return false;
 		
 	}
 }
