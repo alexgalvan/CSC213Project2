595fb50be1a9a3f493ccab0221065874d8122f79
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-23 21:38:33 -0500

    Update Fulltime.java

--- src/Fulltime.java
+++ src/Fulltime.java
@@ -1,37 +1,58 @@
 package src;
 
+/**
+ * Fulltime class,driver from Employee.
+ * This class store profile information and payment of one employee
+ *
+ * @author Yuan zhao,Alexander Galvan
+ */
 public class Fulltime extends Employee {
-	private int salary;
+	private double salary;
 	
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
+	/*
+     * Fulltime Constructor
+     * 
+     * @param profile information about fulltime employee
+     * @param salary for employee
+     */
+	public Fulltime(Profile profile, int salary) {
+		super(profile);
 		this.salary = salary;
 	}
 	
+	/**
+	 * Get salary of fulltime employee
+	 * 
+	 * @return salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+	
+	/**
+	 * Set salary of fulltime employee
+	 * 
+	 * @param salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	/**
+     * Calculate total payment of full time employee
+     */
 	@Override
 	public void calculatePayment() {
 		super.setPayment(getSalary()/26);
 	}
 	
 	@Override
-	public String toString() {
-		return "";
-	}
+    public String toString() {
+        return super.toString() + "FULL TIME" + "::" + "Annual Salary $" + String.format("%,.2f", salary);
+    }
 	
 	@Override
-	public boolean equals(Object obj) { //compare name, department, and date hired
-		if(obj == null)
-			return false;
-		
-		else if(obj instanceof Fulltime) {
-			Fulltime aFulltime = (Fulltime)obj;
-			return salary != aFulltime.getSalary() ? false : true;
-		}
-		
-		else 
-			return false;
-		
-	}
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
